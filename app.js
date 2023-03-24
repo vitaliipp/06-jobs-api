ff63a282ef413076dc6a398d838bcfbe22b85152
vitaliipp <109319234+vitaliipp@users.noreply.github.com>
2023-03-24 17:38:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
-
+app.get('/',(req,res)=>{
+  res.send('jobs api');
+})
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
